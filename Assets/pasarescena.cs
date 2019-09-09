3312d873c7268a2bcf0916afb77d676e2dd2df2d
matias <matias.vera@virginiogomez.cl>
2019-09-09 18:14:36 -0300

    avance

---
+++ Assets/pasarescena.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pasarescena : MonoBehaviour
+{
+    public void Next()
+    {
+        Application.LoadLevel(1);
+
+    }
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
